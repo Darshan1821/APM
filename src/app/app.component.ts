3eb4c6a4f21834259d710ef011c7881e9616a8de
Darshan Bangoria <darshanbangoria.2@gmail.com>
2018-02-03 14:32:10 +0530

    Refactored the application into various modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { ProductService } from './products/product.service';
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ProductService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Acme Product Management';
